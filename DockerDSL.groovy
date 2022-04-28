bc62a5c022c77b9446569b7ed76f44fd4683c627
Luis Gregorio Ruiz Lemas <97902495+LuisRuizLemas@users.noreply.github.com>
2022-04-27 19:16:14 -0700

    Update DockerDSL.groovy

--- DockerDSL.groovy
+++ DockerDSL.groovy
@@ -1,4 +1,4 @@
-job('Aplicacion Node.js Docker DSL') {
+job('Aplicacion Upload Jenkins') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
         git('https://github.com/ctorresso/prac-Docker.git', 'main') { node ->
