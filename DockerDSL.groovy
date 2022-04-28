1491976d3f3308286ad46d5224cdbab960716fde
Luis Gregorio Ruiz Lemas <97902495+LuisRuizLemas@users.noreply.github.com>
2022-04-27 19:10:26 -0700

    Create DockerDSL.groovy

---
+++ DockerDSL.groovy
@@ -0,0 +1,44 @@
+job('Aplicacion Node.js Docker DSL') {
+    description('Aplicación Node JS Docker DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/ctorresso/prac-Docker.git', 'main') { node ->
+            node / gitConfigName('ctorresso')
+            node / gitConfigEmail('a210216376@unison.mx')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('ctorresso/nodejsapp')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
